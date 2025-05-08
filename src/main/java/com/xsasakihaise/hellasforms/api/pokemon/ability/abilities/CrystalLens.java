c0e76c4572df400ed7a72ba0ee1eaf51f4f24cf6
xSasaki_Haise <raehr.dennis@gmail.com>
2025-05-08 02:47:24 +0200

    1.6.7-pre2

--- src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/CrystalLens.java
+++ src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/CrystalLens.java
@@ -1,6 +1,7 @@
 package com.xsasakihaise.hellasforms.api.pokemon.ability.abilities;
 
 import com.pixelmonmod.pixelmon.api.pokemon.ability.AbstractAbility;
+import com.pixelmonmod.pixelmon.api.pokemon.ability.abilities.SuperLuck;
 import com.pixelmonmod.pixelmon.battles.attacks.Attack;
 import com.pixelmonmod.pixelmon.battles.controller.ai.MoveChoice;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
@@ -9,16 +10,8 @@ import com.pixelmonmod.pixelmon.battles.controller.ai.MoveChoice;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import java.util.List;
 
-public class CrystalLens extends AbstractAbility {
+public class CrystalLens extends SuperLuck {
     public boolean ignoreEvasion(PixelmonWrapper pw, PixelmonWrapper target) {
         return true;
-    }
-    public void weightEffect(PixelmonWrapper pw, MoveChoice userChoice, List<MoveChoice> userChoices, List<MoveChoice> bestUserChoices, List<MoveChoice> opponentChoices, List<MoveChoice> bestOpponentChoices) {
-        if (userChoice.isOffensiveMove()) {
-            userChoice.raiseWeight((float)this.stages / 10.0F);
-        } else if (pw.getBattleStats().increaseCritStage(1, false)) {
-            userChoice.raiseWeight(25.0F);
-        }
-
     }
 }
