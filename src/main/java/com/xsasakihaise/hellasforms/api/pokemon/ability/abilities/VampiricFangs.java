8b0078277874dc4317f059df90b778cac9b93f22
xSasaki_Haise <raehr.dennis@gmail.com>
2025-05-10 23:57:55 +0200

    1.7.1b

--- src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/VampiricFangs.java
+++ src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/VampiricFangs.java
@@ -1,41 +1,21 @@
 package com.xsasakihaise.hellasforms.api.pokemon.ability.abilities;
 
-import com.google.common.collect.Lists;
-import com.pixelmonmod.pixelmon.api.battles.AttackCategory;
-import com.pixelmonmod.pixelmon.api.battles.attack.AttackRegistry;
 import com.pixelmonmod.pixelmon.api.pokemon.Element;
-import com.pixelmonmod.pixelmon.api.pokemon.ability.abilities.Absorb;
+import com.pixelmonmod.pixelmon.api.pokemon.ability.AbstractAbility;
 import com.pixelmonmod.pixelmon.battles.attacks.Attack;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
-import java.util.Optional;
 
-public class VampiricFangs extends Absorb {
-    protected VampiricFangs(Element type, String langString) {
-        super(type, langString);
-    }
-
-    public void postProcessAttackOther(PixelmonWrapper pokemon, PixelmonWrapper user, PixelmonWrapper target, Attack a) {
-        if (pokemon != user && !a.fromDancer && a.isAttack(new Optional[]{AttackRegistry.AQUA_STEP, AttackRegistry.FEATHER_DANCE, AttackRegistry.FIERY_DANCE, AttackRegistry.DRAGON_DANCE, AttackRegistry.LUNAR_DANCE, AttackRegistry.PETAL_DANCE, AttackRegistry.REVELATION_DANCE, AttackRegistry.QUIVER_DANCE, AttackRegistry.SWORDS_DANCE, AttackRegistry.SWORDS_DANCE, AttackRegistry.TEETER_DANCE})) {
-            a.fromDancer = true;
-            this.sendActivatedMessage(pokemon);
-            Attack lastUsed = pokemon.lastAttack;
-            PixelmonWrapper newTarget;
-            if (!a.isAttack(new Optional[]{AttackRegistry.TEETER_DANCE}) && !a.isAttack(new Optional[]{AttackRegistry.FEATHER_DANCE})) {
-                if (a.getAttackCategory() == AttackCategory.STATUS) {
-                    newTarget = pokemon;
-                } else if (user.isSameTeam(pokemon)) {
-                    newTarget = target;
-                } else {
-                    newTarget = user;
-                }
+public class VampiricFangs extends AbstractAbility {
+    public boolean allowsIncomingAttack(PixelmonWrapper pokemon, PixelmonWrapper user, Attack a) {
+        if (a.getType() != Element.DARK) {
+            return true;
+        } else {
+            if (a.getType() != Element.GHOST) {
+                return true;
             } else {
-                newTarget = user;
+                pokemon.healByPercent(25.0F);
+                return false;
             }
-
-            pokemon.useTempAttack(a, Lists.newArrayList(new PixelmonWrapper[]{newTarget}), false);
-            pokemon.lastAttack = lastUsed;
-            a.fromDancer = false;
         }
-
     }
 }
