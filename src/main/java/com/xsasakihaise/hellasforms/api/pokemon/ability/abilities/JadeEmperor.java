01ac8d8cb4e0e6d8563dd6afa40a8aae4bfc12e7
xSasaki_Haise <raehr.dennis@gmail.com>
2025-05-19 05:52:04 +0200

    1.7.3-pre1

--- src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/JadeEmperor.java
+++ src/main/java/com/xsasakihaise/hellasforms/api/pokemon/ability/abilities/JadeEmperor.java
@@ -4,6 +4,7 @@ import com.pixelmonmod.pixelmon.api.pokemon.ability.AbstractAbility;
 import com.pixelmonmod.pixelmon.api.pokemon.stats.BattleStatsType;
 import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
 import com.pixelmonmod.pixelmon.battles.status.MistyTerrain;
+import com.pixelmonmod.pixelmon.battles.status.PsychicTerrain;
 import com.pixelmonmod.pixelmon.battles.status.Terrain;
 import com.pixelmonmod.pixelmon.enums.heldItems.EnumHeldItems;
 
@@ -13,12 +14,12 @@ public class JadeEmperor extends AbstractAbility {
 
     public void applySwitchInEffect(PixelmonWrapper newPokemon) {
         Terrain terrain = newPokemon.bc.globalStatusController.getTerrain();
-        if (!(terrain instanceof MistyTerrain)) {
+        if (!(terrain instanceof PsychicTerrain)) {
             if (terrain != null) {
                 newPokemon.bc.globalStatusController.removeGlobalStatus(terrain);
             }
 
-            MistyTerrain et = new MistyTerrain(newPokemon.getUsableHeldItem().getHeldItemType() == EnumHeldItems.terrainExtender);
+            PsychicTerrain et = new PsychicTerrain(newPokemon.getUsableHeldItem().getHeldItemType() == EnumHeldItems.terrainExtender);
             newPokemon.bc.sendToAll(et.langStart, new Object[0]);
             newPokemon.addGlobalStatus(et);
         }
