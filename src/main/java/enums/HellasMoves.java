ec42f33240cdac598695a90ed3eb451f76239942
xSasaki_Haise <raehr.dennis@gmail.com>
2025-06-29 04:01:47 +0200

    1.9.0_hotfix04

--- src/main/java/enums/HellasMoves.java
+++ src/main/java/enums/HellasMoves.java
@@ -1,9 +1,11 @@
 package enums;
 
+import battles.attacks.specialAttacks.basic.DivineCastigation;
 import com.pixelmonmod.pixelmon.api.pokemon.Element;
 
 public enum HellasMoves {
-    Corrode(1, "Corrode", Element.POISON);
+    Corrode(1, "Corrode", Element.POISON),
+    DivineCastigation(2, "Divine Castigation", Element.FIGHTING);
 
     private final int id;
     private final String moveName;
