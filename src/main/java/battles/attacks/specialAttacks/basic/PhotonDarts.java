7662de22d5eefcaad4f86f23e09b011499e67c1e
xSasaki_Haise <raehr.dennis@gmail.com>
2025-07-26 06:04:19 +0200

    1.9.2

---
+++ src/main/java/battles/attacks/specialAttacks/basic/PhotonDarts.java
@@ -0,0 +1,12 @@
+package battles.attacks.specialAttacks.basic;
+
+import com.pixelmonmod.pixelmon.battles.attacks.Attack;
+import com.pixelmonmod.pixelmon.battles.attacks.DamageTypeEnum;
+import com.pixelmonmod.pixelmon.battles.attacks.specialAttacks.basic.SpecialAttackBase;
+import com.pixelmonmod.pixelmon.battles.controller.participants.PixelmonWrapper;
+
+public class PhotonDarts extends SpecialAttackBase {
+    public void dealtDamage(PixelmonWrapper attacker, PixelmonWrapper defender, Attack attack, DamageTypeEnum damageType)
+    {
+    }
+}
