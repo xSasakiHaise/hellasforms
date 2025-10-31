ae6b021d6a0a95ac6b2fd1ba980b30257a370170
xSasakiHaise <raehr.dennis@gmail.com>
2025-10-31 23:51:16 +0100

    Add cross-version Pixelmon stat compatibility

--- src/main/java/com/xsasakihaise/hellasforms/items/consumables/RustedBottleCapItem.java
+++ src/main/java/com/xsasakihaise/hellasforms/items/consumables/RustedBottleCapItem.java
@@ -1,7 +1,6 @@
 package com.xsasakihaise.hellasforms.items.consumables;
 
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
-import com.pixelmonmod.pixelmon.api.pokemon.stats.StatType;
 import com.xsasakihaise.hellasforms.util.PixelmonStatTypes;
 import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -12,21 +11,21 @@ import net.minecraft.util.text.TranslationTextComponent;
 import java.util.Random;
 
 public class RustedBottleCapItem extends PokemonInteractItem {
-    private static final StatType[] BATTLE_STATS = new StatType[]{
-            StatType.HP,
-            StatType.ATTACK,
+    private static final Enum<?>[] BATTLE_STATS = new Enum[]{
+            PixelmonStatTypes.hp(),
+            PixelmonStatTypes.attack(),
             PixelmonStatTypes.defence(),
-            StatType.SPECIAL_ATTACK,
+            PixelmonStatTypes.specialAttack(),
             PixelmonStatTypes.specialDefence(),
-            StatType.SPEED
+            PixelmonStatTypes.speed()
     };
 
     private final Random random = new Random();
 
     @Override
     protected boolean applyEffect(PlayerEntity player, Pokemon pokemon, PixelmonEntity entity, ItemStack stack) {
-        StatType chosen = BATTLE_STATS[random.nextInt(BATTLE_STATS.length)];
-        pokemon.getIVs().setStat(chosen, 0);
+        Enum<?> chosen = BATTLE_STATS[random.nextInt(BATTLE_STATS.length)];
+        PixelmonStatTypes.setIV(pokemon.getIVs(), chosen, 0);
         pokemon.markDirty();
         return true;
     }
