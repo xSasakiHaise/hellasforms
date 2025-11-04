6cde8616c184384acfaeb76d6d1abf713a7d9586
xSasaki_Haise <raehr.dennis@gmail.com>
2025-11-04 01:24:00 +0100

    2.7.0

--- src/main/java/battles/terrain/nightmarerealm/NightmareTerrain.java
+++
@@ -1,6 +0,0 @@
-package battles.terrain.nightmarerealm;
-
-public final class NightmareTerrain {
-    public static final String ID = "nightmare_terrain";
-    private NightmareTerrain() {}
-}
--- src/main/java/battles/weather/nightmarerealm/NightmareWeather.java
+++
@@ -1,6 +0,0 @@
-package battles.weather.nightmarerealm;
-
-public final class NightmareWeather {
-    public static final String ID = "nightmare_weather";
-    private NightmareWeather() {}
-}
